350f02a031237e03d893a0ec154c0317deebed7c
Tom Vaughan <tomvaughan77@gmail.com>
2023-04-05 10:28:23 +0100

    Full-size chat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,11 @@ import { SocketProvider } from '../components/socket/socket'
 import Header from '~/components/header/Header'
 import '~/styles/globals.css'
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-    return (
-        <SocketProvider>
-            <Header />
-            <Component {...pageProps} />
-        </SocketProvider>
-    )
-}
+const MyApp: AppType = ({ Component, pageProps }) => (
+    <SocketProvider>
+        <Header />
+        <Component {...pageProps} />
+    </SocketProvider>
+)
 
 export default MyApp
