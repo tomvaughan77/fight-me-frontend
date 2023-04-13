72066fcad96436a97ea1fea9db57f0fdfef722fd
Tom Vaughan <tomvaughan77@gmail.com>
2023-04-13 12:06:30 +0100

    Simple play tests

--- src/components/chat/TopicLabel.stories.tsx
+++ src/components/chat/TopicLabel.stories.tsx
@@ -1,42 +1,69 @@
-import TopicLabel, { TopicLabelProps } from './TopicLabel'
+import { expect } from '@storybook/jest'
+import type { StoryObj } from '@storybook/react'
+import { within } from '@storybook/testing-library'
+import TopicLabel from './TopicLabel'
 
 export default {
     component: TopicLabel,
     title: 'Components/Chat/TopicLabel',
 }
 
-const Template = {
-    render: (args: TopicLabelProps) => <TopicLabel {...args} />,
+const Template: StoryObj<typeof TopicLabel> = {
+    render: (args) => <TopicLabel {...args} />,
 }
 
-export const ForTopic = {
+export const ForTopic: typeof Template = {
     ...Template,
     args: {
         topic: 'Example topic',
         side: 'true',
     },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+
+        await expect(canvas.getByText('FOR')).toBeInTheDocument()
+        await expect(canvas.getByText(/Example topic/)).toBeInTheDocument()
+    },
 }
 
-export const AgainstTopic = {
+export const AgainstTopic: typeof Template = {
     ...Template,
     args: {
         topic: 'Example topic',
         side: 'false',
     },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+
+        await expect(canvas.getByText('AGAINST')).toBeInTheDocument()
+        await expect(canvas.getByText(/Example topic/)).toBeInTheDocument()
+    },
 }
 
-export const ForEmpty = {
+export const ForEmpty: typeof Template = {
     ...Template,
     args: {
         topic: '',
         side: 'true',
     },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+
+        await expect(canvas.getByText('FOR')).toBeInTheDocument()
+        await expect(canvas.queryByText(/Example topic/)).not.toBeInTheDocument()
+    },
 }
 
-export const AgainstEmpty = {
+export const AgainstEmpty: typeof Template = {
     ...Template,
     args: {
         topic: '',
         side: 'false',
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+
+        await expect(canvas.getByText('AGAINST')).toBeInTheDocument()
+        await expect(canvas.queryByText(/Example topic/)).not.toBeInTheDocument()
     },
 }
