c06f21ecc2641682d2ea3ebac7fca9d0a5bed56c
Tom Vaughan <tomvaughan77@gmail.com>
2023-04-14 16:29:02 +0100

    Exploding sheep

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ThemePicker from './ThemePicker'
+import Image from 'next/image'
+import SheepExplosionLogo from 'public/assets/Sheep_Explosion.png'
 
 const Header: React.FC = () => {
     return (
         <header className="navbar bg-base-100">
             <div className="flex-1">
                 <a className="btn btn-ghost normal-case text-2xl font-bold">Fight.Me</a>
+                <Image src={SheepExplosionLogo} alt={'Sheep explosion logo'} width={100} height={100} />
             </div>
             <div className="flex-none gap-2">
                 <ThemePicker />
