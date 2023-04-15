5be1cfd138d7285cd7519423c8c9394c2c83e61d
Tom Vaughan <tomvaughan77@gmail.com>
2023-04-15 10:08:07 +0100

    Tiny TopicLabel refactor

--- src/components/chat/TopicLabel.tsx
+++ src/components/chat/TopicLabel.tsx
@@ -3,17 +3,13 @@ export interface TopicLabelProps {
     side: string
 }
 
-const TopicLabel: React.FC<TopicLabelProps> = ({ topic, side }) => {
-    console.log(`Side: ${side}`)
-    console.log(`Value: ${side === 'true'}`)
-    return (
-        <>
-            You are arguing {side === 'true' ? <SidelabelFor /> : <SidelabelAgainst />} {topic}
-        </>
-    )
-}
+const TopicLabel: React.FC<TopicLabelProps> = ({ topic, side }) => (
+    <>
+        You are arguing {side === 'true' ? <SideLabelFor /> : <SideLabelAgainst />} {topic}
+    </>
+)
 
-const SidelabelFor = () => <div className="badge badge-success gap-2">FOR</div>
-const SidelabelAgainst = () => <div className="badge badge-error gap-2">AGAINST</div>
+const SideLabelFor = () => <div className="badge badge-success gap-2">FOR</div>
+const SideLabelAgainst = () => <div className="badge badge-error gap-2">AGAINST</div>
 
 export default TopicLabel
