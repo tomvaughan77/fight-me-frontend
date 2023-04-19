e0b617cb6f53a61fbec698b6967144fa0329b0f1
Tom Vaughan <tomvaughan77@gmail.com>
2023-04-19 14:07:22 +0100

    Added Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { type AppType } from 'next/dist/shared/lib/utils'
-
+import { Analytics } from '@vercel/analytics/react'
 import Header from '~/components/header/Header'
 import SocketProvider from '~/hooks/socket/socketContext'
 import '~/styles/globals.css'
@@ -9,6 +9,7 @@ const MyApp: AppType = ({ Component, pageProps }) => (
         <SocketProvider>
             <Header />
             <Component {...pageProps} />
+            <Analytics />
         </SocketProvider>
     </>
 )
