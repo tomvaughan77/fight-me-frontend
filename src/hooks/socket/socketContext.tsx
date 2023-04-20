cd3f05e8ef06ddff1c7c78ea25bb72875d7b62a2
Tom Vaughan <tomvaughan77@gmail.com>
2023-04-20 10:21:28 +0100

    Fixed env var getting

--- src/hooks/socket/socketContext.tsx
+++ src/hooks/socket/socketContext.tsx
@@ -1,8 +1,10 @@
 import React, { createContext, type ReactNode } from 'react'
 import { io } from 'socket.io-client'
 
-const url = process.env.BACKEND_URL ?? 'http://localhost'
-const port = String(process.env.BACKED_PORT) ?? '5000'
+console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
+
+const url = process.env.NEXT_PUBLIC_BACKEND_URL ? process.env.NEXT_PUBLIC_BACKEND_URL : 'http://localhost'
+const port = process.env.NEXT_PUBLIC_BACKEND_URL ? String(process.env.NEXT_PUBLIC_BACKEND_URL) : '5000'
 const socket = io(`${url}:${port}`)
 
 export const SocketContext = createContext(socket)
